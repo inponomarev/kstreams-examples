74d603f49277900968f908d3a063857286ab9fcf
inponomarev <iponomarev@mail.ru>
2019-01-29 21:19:58 +0300

    tables example cleanup

--- tables/src/main/java/ru/curs/counting/configuration/TopologyConfiguration.java
+++ tables/src/main/java/ru/curs/counting/configuration/TopologyConfiguration.java
@@ -3,10 +3,7 @@ package ru.curs.counting.configuration;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.ValueJoiner;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,30 +21,30 @@ public class TopologyConfiguration {
                         Consumed.with(Serdes.String(), Serdes.String())
                 ).filter((k, v) -> v.toLowerCase().contains("jpoint"));
 
-
+        filtered.print(Printed.toSysOut());
 //Counter
         KTable<String, Long> count = filtered.groupByKey().count();
         count.toStream().foreach((k, v) -> {
             gui.update(k, String.valueOf(v));
         });
-
 /*
         KTable<String, String> latest = filtered.groupByKey()
                 .reduce((v1, v2) -> v2);
-
-       //latest tweet
-       latest.toStream().foreach((k, v) -> {
+*/
+        //latest tweet
+   /*    latest.toStream().foreach((k, v) -> {
             gui.update(k, v);
         });
-
-
+*/
+/*
        KTable<String, String> geo = streamsBuilder.table("geo", Consumed.with(Serdes.String(), Serdes.String()));
         KTable<String, String> joined = latest.leftJoin(geo, (v1, v2) -> String.format("%s: %s", v2, v1));
 
         joined.toStream().foreach((k, v) -> {
             gui.update(k, v);
         });
-
+*/
+        /*
 //Latest tweet - via topic
         filtered.to("filtered", Produced.with(Serdes.String(), Serdes.String()));
 
