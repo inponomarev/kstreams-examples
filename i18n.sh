d70dfb483877da1f907226190f65daad68f29d95
fiddlededee <new@mail.com>
2022-07-15 12:38:48 +0300

    Added i18n

---
+++ i18n.sh
@@ -0,0 +1,15 @@
+LDIRS="lecture01 lecture02"
+
+for LDIR in $LDIRS
+do
+  echo
+  echo "--------------------------------------------------------------------------------"
+  echo "Processing  $LDIR"
+  echo "--------------------------------------------------------------------------------"
+  echo
+  pushd $LDIR/src/main/asciidoc || exit
+  txt2po -P kstreams.adoc kstreams.pot
+  msgmerge -U kstreams-en.po kstreams.pot
+  po2txt -t kstreams.adoc kstreams-en.po kstreams-en.adoc
+  popd || exit
+done
